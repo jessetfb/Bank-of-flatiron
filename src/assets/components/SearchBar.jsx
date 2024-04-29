1f0205f650bbc88dd9619a7a95e8196cd9594d14
jessetfb <langatjesse@gmail.com>
2024-04-29 09:10:40 +0300

    done

---
+++ src/assets/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import { FaSearch } from 'react-icons/fa';
+
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleChange(event) {
+    onSearch(event.target.value);
+  }
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div>
+      <input style={{marginRight: '20px', padding: '10px', borderRadius: '5px', width: '300px'}}
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="Enter search term..."
+      />
+      <button onClick={handleSearch}
+      style={{padding: '10px', color: 'black', cursor: 'pointer', borderRadius: '5px'}}>
+        <FaSearch />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
